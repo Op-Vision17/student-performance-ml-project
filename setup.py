fe3e5b66ba068b399ae700aee42b31097a49f389
Arjun Gupta <guptaarjun1711@gmail.com>
2025-05-20 17:43:19 +0530

    setup done

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages, setup
 from typing import List
 HYPHEN_E_DOT = "-e ."
-def get_requirements(file_path: str) -> list[str]:
+def get_requirements(file_path: str) -> List[str]:
+
     '''
     This function returns a list of requirements
     '''
